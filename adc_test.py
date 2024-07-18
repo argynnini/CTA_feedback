e7405aba2619b52838c29129844cdf7965555433
argynnini <argynnini@gmail.com>
2024-07-19 02:46:39 +0900

    get CTA Resistance

--- adc_test.py
+++ adc_test.py
@@ -4,57 +4,73 @@ import os
 import pigpio
 import csv
 from simple_pid import PID
+import math
 
-Kp = 0.1
-Ki = 0.01
-Kd = 0.05
+Kp = 0
+Ki = 0.0
+Kd = 0.0
 
-pid = PID(Kp, Ki, Kd, setpoint=0)
-pid.output_limits = (0, 30) # PWM は0~30%の範囲で出力
+pid = PID(-Kp, -Ki, -Kd, setpoint=0)
+pid.output_limits = (0, 30)  # PWM は0~30%の範囲で出力
+pid.sample_time = 0.1  # 制御周期は0.1秒
+# pid.proportional_on_measurement = True  # 逓減制御
 
-
+test_distance = [4, 7]  # , 10, 13, 16, 19, 22, 25]
+test_resistance = [
+    0.39 * math.sqrt(25.36 - x) + 7.4 for x in test_distance
+]  # テスト抵抗値
 
 pwm_freq = 1000
-duty_cycle = 0
+duty_range = [0, 30]  # duty比の範囲
+
+diode_current = 0.0963  # 96.3mA
 
 gpio = pigpio.pi()
 
 v_thresh = [1.5, 3.0]
 
-# 5Vでは2MHzが限界?
-try: 
-    adc.open(port=0, cs=0, speed=2000000) # 2 MHz が限界
+# 5Vでは2MHzが限界
+try:
+    adc.open(port=0, cs=0, speed=2000000)  # 5V のとき 2 MHz が限界
 # 例外処理(権限がない場合)
 except PermissionError:
     os.system("sudo groupadd gpio")
     os.system("sudo usermod -a -G gpio -G spi pi")
     os.system("sudo chown root:spi /dev/spidev*")
     os.system("sudo chmod g+rw /dev/spidev*")
-    adc.open(port=0, cs=0, speed=7800000) # 7.8 MHz が限界
+    adc.open(port=0, cs=0, speed=2000000)  # 5V のとき 2 MHz が限界
 
 # 上限は30%くらい
-
-value = []
-start_time = time.time()
-with open('/home/pi/BMF_CV/idiode.csv', 'w', newline='') as file:
+csv_data = []
+control = 0
+start_time = time.perf_counter()
+with open("/home/pi/BMF_CV/idiode.csv", "w", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["time", "Voltage"])
+    writer.writerow(["time", "Voltage", "Resistance", "PWM"])
     try:
-        gpio.hardware_PWM(12, pwm_freq, int(duty_cycle * 255 / 100))
+        gpio.hardware_PWM(12, pwm_freq, int(0 * 255 / 100))
         gpio.set_PWM_dutycycle(12, int(10 * 255 / 100))
+        pid.setpoint = test_resistance[0]
+        print("target: {0:.3f}Ω".format(pid.setpoint))
         while True:
-            voltage = adc.read_voltage(7)
-            elapsed_time = time.time() - start_time
-            print("time: {0:.3f}, voltage: {1:.3f}".format(elapsed_time, voltage), end="\r")
+            voltage = adc.read_voltage(channel=6, convtype=0, vref=5.0)
+            elapsed_time = time.perf_counter() - start_time
             if voltage > v_thresh[0] and voltage < v_thresh[1]:
-                value.append([elapsed_time, voltage])
+                if (control := pid(voltage / diode_current)) is None:
+                    control = 0
+                gpio.set_PWM_dutycycle(12, int(control * 255 / 100))
+                csv_data.append([elapsed_time, voltage, voltage / diode_current, control])
+            print(
+                "T: {0:.1f}s, V: {1:.3f}V, R: {2:.1f}Ω, PWM: {3:.1f}%".format(elapsed_time, voltage, voltage / diode_current, control),
+                end="\r",
+            )
 
     # キーボード割り込み(Ctrl + C)があった場合の処理
     except KeyboardInterrupt:
         gpio.set_PWM_dutycycle(12, int(0 * 255 / 100))
-        gpio.stop()
-        print("GPIO cleanup")
-        writer.writerows(value)
-        raise
-
-
+        print("\nGPIO cleanup")
+        # 経過時間の最初を0にする
+        for i in range(len(csv_data)):
+            csv_data[i][0] = csv_data[i][0] - csv_data[0][0]
+        writer.writerows(csv_data)
+        exit(gpio.stop())
--- rotate_saikyo_pid_pwm.py
+++ rotate_saikyo_pid_pwm.py
@@ -1,6 +1,7 @@
 """
 BMFに流れる電流とBMFにかかる電圧センサデータを連続収集して抵抗値を求めCSVに保存
 """
+
 # 取ってsample数たまったら平均
 """
 sudo groupadd gpio
@@ -23,59 +24,66 @@ import rotary_code as rc
 
 # --- パラメータ設定 ---
 # /// PID ///
-Kp = 40.0 # 比例ゲイン40 :y = 24.286x + 18.333 : y=41.182e^(0.2377x) : y=4.4643x^2-6.9643x+60
-Ki = 10.0 # 積分ゲイン 10
-Kd = 0.50 # 微分ゲイン 0.5
+Kp = 40.0  # 比例ゲイン40 :y = 24.286x + 18.333 : y=41.182e^(0.2377x) : y=4.4643x^2-6.9643x+60
+Ki = 10.0  # 積分ゲイン 10
+Kd = 0.50  # 微分ゲイン 0.5
 # /// PWM ///
-pwm_freq = 1000 # 1000
-test_distance = [4,7,10,13,16,19,22,25]
-test_resistance = [0.39 * math.sqrt(25.36 - x) + 7.4 for x in test_distance] # テスト抵抗値
+pwm_freq = 1000  # 1000
+test_distance = [4, 7, 10, 13, 16, 19, 22, 25]
+test_resistance = [
+    0.39 * math.sqrt(25.36 - x) + 7.4 for x in test_distance
+]  # テスト抵抗値
 # test_resistance = # list(np.linspace(9.4, 7.4, 10)) # [9.4, 9.0, 8.5, 8.0, 7.5] # テスト抵抗値
-test_interval = 10 # テスト間隔(sec)
-resistance_range = [7.4, 9.4] # 抵抗値の許容範囲(7.5~10.0Ω)
-duty_range = [7, 100] # PWMの許容範囲(0~100%)7
-pins = [22, 23, 24, 25] # ロータリコードのピン設定
+test_interval = 10  # テスト間隔(sec)
+resistance_range = [7.4, 9.4]  # 抵抗値の許容範囲(7.5~10.0Ω)
+duty_range = [7, 100]  # PWMの許容範囲(0~100%)7
+pins = [22, 23, 24, 25]  # ロータリコードのピン設定
 # /// その他 ///
-isSave = True # csvに保存するかどうか
-read_samples = 500 # サンプル数 500
-Vthreshold = 1.0 # 電圧の閾値(これよりちいさいとデータを取得しない)
-Vref = 5.0 # 電源電圧
-Offset = 2.5 # オフセット電圧
+isSave = True  # csvに保存するかどうか
+read_samples = 500  # サンプル数 500
+Vthreshold = 1.0  # 電圧の閾値(これよりちいさいとデータを取得しない)
+Vref = 5.0  # 電源電圧
+Offset = 2.5  # オフセット電圧
 
 # --- 定数計算 ---
-work_dir = os.getcwd() # 作業ディレクトリのパス
+work_dir = os.getcwd()  # 作業ディレクトリのパス
 
 # 抵抗値の範囲
 r_range = resistance_range[1] - resistance_range[0]
 
 # 現在時刻取得
 t_delta = datetime.timedelta(hours=9)
-JST = datetime.timezone(offset=t_delta, name='JST')
+JST = datetime.timezone(offset=t_delta, name="JST")
 now = datetime.datetime.now(JST)
 
 # --- ロゴ表示 ---
-os.system(f"clear && paste {work_dir}/skyfish/pilogo.txt {work_dir}/skyfish/bmflogo.txt | lolcat")
+os.system(
+    f"clear && paste {work_dir}/skyfish/pilogo.txt {work_dir}/skyfish/bmflogo.txt | lolcat"
+)
+
+
 # os.system(f"clear && paste {work_dir}/skyfish/pilogo.txt {work_dir}/skyfish/bmflogo.txt | tte slide")
 # tteに渡す引数一覧（https://chrisbuilds.github.io/terminaltexteffects/showroom/）
 # --- ADC設定 ---
 # チャンネル定義
 class ch(IntEnum):
     # Configure ADC settings
-    BMF_A = 0 # BMFに流れる電流
-    BMF_V = 2 # BMFにかかる電圧
-    PWR_3V3 = 4 # 3.3V電源電圧
-    PWR_5V0 = 6 # 5.0V電源電圧
+    BMF_A = 0  # BMFに流れる電流
+    BMF_V = 2  # BMFにかかる電圧
+    PWR_3V3 = 4  # 3.3V電源電圧
+    PWR_5V0 = 6  # 5.0V電源電圧
+
 
 # 5Vでは2MHzが限界?
-try: 
-    adc.open(port=0, cs=0, speed=7800000) # 7.8 MHz が限界
+try:
+    adc.open(port=0, cs=0, speed=7800000)  # 7.8 MHz が限界
 # 例外処理(権限がない場合)
 except PermissionError:
     os.system("sudo groupadd gpio")
     os.system("sudo usermod -a -G gpio -G spi pi")
     os.system("sudo chown root:spi /dev/spidev*")
     os.system("sudo chmod g+rw /dev/spidev*")
-    adc.open(port=0, cs=0, speed=7800000) # 7.8 MHz が限界
+    adc.open(port=0, cs=0, speed=7800000)  # 7.8 MHz が限界
 
 # --- PWM ---
 pi = pigpio.pi()
@@ -89,9 +97,13 @@ except AttributeError:
 rc.setup_pin(pi, pins, pigpio.INPUT, pigpio.PUD_DOWN)
 
 # --- PID制御 ---
-pid = PID(-Kp, -Ki, -Kd,
-          setpoint=((1 - (rc.read_pin(pi, pins) / 9.0)) * r_range) + resistance_range[0],
-          output_limits=(duty_range[0], duty_range[1]))
+pid = PID(
+    -Kp,
+    -Ki,
+    -Kd,
+    setpoint=((1 - (rc.read_pin(pi, pins) / 9.0)) * r_range) + resistance_range[0],
+    output_limits=(duty_range[0], duty_range[1]),
+)
 
 # --- CSV ---
 # CSVファイル名
@@ -105,7 +117,13 @@ csv_header = ["elapsed_S", "supply_V", "current_A", "BMF_R", "target_R", "PWM"]
 try:
     # CSV保存用データ
     csv_data = np.empty((0, len(csv_header)), dtype=np.float64)
-    print("テスト抵抗値: ", test_resistance, "予想時間: ", len(test_resistance) * test_interval, "秒")
+    print(
+        "テスト抵抗値: ",
+        test_resistance,
+        "予想時間: ",
+        len(test_resistance) * test_interval,
+        "秒",
+    )
     seikaku_count = 0
 
     # 開始時間
@@ -114,12 +132,14 @@ try:
     start = time.perf_counter()
     elapse = 0
     while time.perf_counter() - start < len(test_resistance) * test_interval:
-        pid.setpoint = target_resistance = test_resistance[int((time.perf_counter() - start) / test_interval) % len(test_resistance)]
+        pid.setpoint = target_resistance = test_resistance[
+            int((time.perf_counter() - start) / test_interval) % len(test_resistance)
+        ]
         # pid.Kp = -1*41.182 * np.exp(0.2377 * target_resistance)
         # pid.Kp = -4.4643 * (target_resistance ** 2) + 6.9643 * target_resistance - 60
         # キャッシュデータ (BMF電圧, BMF電流)
         cache_data = np.empty((0, 2), dtype=np.float64)
-        while len(cache_data) < read_samples: # サンプル数分だけ取得
+        while len(cache_data) < read_samples:  # サンプル数分だけ取得
             # 電竜と電圧を取得
             voltage = adc.read_voltage(ch.BMF_V, adc.conv.diff, Vref)
             current = (adc.read_voltage(ch.BMF_A, adc.conv.sgl, Vref) - Offset) / Vref
@@ -148,29 +168,40 @@ try:
         # 平均値を取得
         mean = np.mean(cache_data, axis=0)
         # 抵抗値の算出 (R = V / I)
-        resistance = np.divide(mean[0], mean[1], out=np.zeros_like(mean[0]), where=mean[1] != 0.0)
+        resistance = np.divide(
+            mean[0], mean[1], out=np.zeros_like(mean[0]), where=mean[1] != 0.0
+        )
         if abs(resistance - target_resistance) < 0.1:
             seikaku_count += 1
         # PID値計算
-        output = pid(resistance) # 0-100%
+        output = pid(resistance)  # 0-100%
         if output is None:
             output = 0.0
         # PWM制御
-        pi.set_PWM_dutycycle(12, int(output * 2.55)) # 0-255
+        pi.set_PWM_dutycycle(12, int(output * 2.55))  # 0-255
 
         # 情報開示
-        sa = ((f'\033[32m' if abs(resistance - target_resistance) < 0.1 else
-              f'\033[34m' if resistance > target_resistance else
-              f'\033[31m') +
-              f'{resistance:02.2f}\033[0m/{target_resistance:02.2f}')
-        print(f'S:{elapse:4.2f}s', f'\033[32m{seikaku_count}\033[0m',
-              f'P:{pid.components[0]:+03.1f}', f'I:{pid.components[1]:+03.1f}', f'D:{pid.components[2]:+03.1f}',
-              f'PWM:{int(output):03d}',
-              f'OHM:{sa}Ω', end="\r")
+        sa = (
+            f"\033[32m"
+            if abs(resistance - target_resistance) < 0.1
+            else f"\033[34m" if resistance > target_resistance else f"\033[31m"
+        ) + f"{resistance:02.2f}\033[0m/{target_resistance:02.2f}"
+        print(
+            f"S:{elapse:4.2f}s",
+            f"\033[32m{seikaku_count}\033[0m",
+            f"P:{pid.components[0]:+03.1f}",
+            f"I:{pid.components[1]:+03.1f}",
+            f"D:{pid.components[2]:+03.1f}",
+            f"PWM:{int(output):03d}",
+            f"OHM:{sa}Ω",
+            end="\r",
+        )
 
         # CSVキャッシュ書き込み
-        csv_data = np.concatenate((csv_data, [[elapse, *mean, resistance, target_resistance, output]]), axis=0)
-    
+        csv_data = np.concatenate(
+            (csv_data, [[elapse, *mean, resistance, target_resistance, output]]), axis=0
+        )
+
     # 終了処理
     print("\nADCを閉じています.")
     adc.close()
@@ -181,9 +212,16 @@ try:
     # CSVに保存
     if isSave:
         print(csv_filename + ".csvに保存中.")
-        np.savetxt(f'{work_dir}/{csv_filename}.csv', np.array(csv_data), delimiter=',', header= ','.join (csv_header) + f',#Kp:{Kp},#Ki:{Ki},#Kd:{Kd},#avg:{read_samples},#freq:{pwm_freq},#duty:[{duty_range[0]}-{duty_range[1]}],#Vth:{Vthreshold},{now.strftime("%Y/%m/%d-%H:%M:%S")}', comments='')
+        np.savetxt(
+            f"{work_dir}/{csv_filename}.csv",
+            np.array(csv_data),
+            delimiter=",",
+            header=",".join(csv_header)
+            + f',#Kp:{Kp},#Ki:{Ki},#Kd:{Kd},#avg:{read_samples},#freq:{pwm_freq},#duty:[{duty_range[0]}-{duty_range[1]}],#Vth:{Vthreshold},{now.strftime("%Y/%m/%d-%H:%M:%S")}',
+            comments="",
+        )
         print("「\033[31m" + csv_filename + ".csv\033[0m」に保存しました.")
-    print('終了\033[?25h')
+    print("終了\033[?25h")
 
 except KeyboardInterrupt:
     print("\nADCを閉じています.")
@@ -195,6 +233,13 @@ except KeyboardInterrupt:
     # CSVに保存
     if isSave:
         print(csv_filename + ".csvに保存中.")
-        np.savetxt(f'{work_dir}/{csv_filename}.csv', np.array(csv_data), delimiter=',', header= ','.join (csv_header) + f',#Kp:{Kp},#Ki:{Ki},#Kd:{Kd},#avg:{read_samples},#freq:{pwm_freq},#duty:[{duty_range[0]}-{duty_range[1]}],#Vth:{Vthreshold},{now.strftime("%Y/%m/%d-%H:%M:%S")}', comments='')
+        np.savetxt(
+            f"{work_dir}/{csv_filename}.csv",
+            np.array(csv_data),
+            delimiter=",",
+            header=",".join(csv_header)
+            + f',#Kp:{Kp},#Ki:{Ki},#Kd:{Kd},#avg:{read_samples},#freq:{pwm_freq},#duty:[{duty_range[0]}-{duty_range[1]}],#Vth:{Vthreshold},{now.strftime("%Y/%m/%d-%H:%M:%S")}',
+            comments="",
+        )
         print("「\033[31m" + csv_filename + ".csv\033[0m」に保存しました.")
-    print('終了\033[?25h')
+    print("終了\033[?25h")
